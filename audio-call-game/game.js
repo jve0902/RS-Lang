ed04a59293f5f7e1b58dbb9f60b97599adfa2bff
jve0902 <jay090218@gmail.com>
2020-06-23 16:31:48 +0300

    feat:add game logic and change some layout

--- audio-call-game/game.js
+++ audio-call-game/game.js
@@ -1,30 +1,30 @@
 const question = document.getElementById('question');
+const transcription = document.getElementById('transcription');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 const btn = document.querySelector('.btn');
-
-let currentQuestion, availableQuestions;
+const iconCircle = document.querySelector('.icon-circle');
+const finalResult = document.querySelector('.final_result');
+const questionCounterText = document.getElementById('questionCounter');
+const scoreCounterText = document.getElementById('score');
+let questions, currentQuestion, availableQuestions;
 let score = 0;
 let questionCounter = 0;
+let bonus = 5;
 
-let questions = [];
+const randomInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
 
-function randomInteger(min, max) {
-  const rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
+const fetchWords = () => {
+  fetch(`https://afternoon-falls-25894.herokuapp.com/words?page=${randomInteger(0, 19)}&group=${randomInteger(0, 5)}`)
+  .then(res => res.json())
+  .then(loadedQuestions => {
+    console.log(loadedQuestions)
+    questions = loadedQuestions;
+    startGame();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 }
-
-fetch(`https://afternoon-falls-25894.herokuapp.com/words?page=${randomInteger(0, 5)}&group=${randomInteger(0, 19)}`)
-.then(res=>{
-  return res.json();
-})
-.then(loadedQuestions => {
-  console.log(loadedQuestions)
-  questions = loadedQuestions;
-  startGame();
-})
-.catch(err=>{
-  console.error(err);
-});
 
 const startGame = () => {
   questionCounter = 0;
@@ -39,17 +39,23 @@ const chooseVariant = (e) => {
   const rightAnswer = currentQuestion.wordTranslate;
 
   if (selectedAnswer == rightAnswer) {
+    incrementScore(bonus);
     choices.forEach((choice) => {
       choice.classList.add('disable');
       question.innerHTML = currentQuestion.word;
+      transcription.innerHTML = currentQuestion.transcription;
+      iconCircle.classList.add('changeSize');
       choice.removeEventListener('click', chooseVariant);
     })
     selectedChoice.classList.remove('disable');
     selectedChoice.classList.add('correct');
-  } /*else {
+  } else {
     choices.forEach((choice) => {
-      if (choice.dataset['number'] == rightAnswer) {
+      if (choice.innerHTML === rightAnswer) {
         choice.classList.add('correct');
+        iconCircle.classList.add('changeSize');
+        question.innerHTML = currentQuestion.word;
+        transcription.innerHTML = currentQuestion.transcription;
       } else {
         choice.classList.add('disable');
       }
@@ -57,7 +63,7 @@ const chooseVariant = (e) => {
       selectedChoice.classList.remove('disable');
       choice.removeEventListener('click', chooseVariant);
     })
-  }*/
+  }
   btn.innerHTML = 'Next';
   btn.removeEventListener('click', dontKnowAction);
   btn.addEventListener('click', getNewQuestion);
@@ -65,29 +71,36 @@ const chooseVariant = (e) => {
 
 const getNewQuestion = () => {
   if (availableQuestions.length === 0) {
+    
     return window.location.assign('/end.html');
   }
   questionCounter++;
-
+  questionCounterText.innerText = `${questionCounter}/20`;
+  scoreCounterText.innerText = `${score}/100`;
   btn.removeEventListener('click', getNewQuestion);
   btn.addEventListener('click', dontKnowAction);
   btn.innerHTML = 'Не знаю';
+  iconCircle.classList.remove('changeSize');
   const questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerHTML = "";
-
- var audio = new Audio();
-  audio.src = currentQuestion.audio;
-  audio.controls = true;
-  audio.autoplay = true;
-  document.body.appendChild(audio);
-
-  
-  choices.forEach((choice) => {
-    console.log(choice)
-    //let random = Math.floor(Math.random() * availableQuestions.length);
-    choice.innerHTML = currentQuestion.wordTranslate;
-    
+  transcription.innerHTML = "";
+  fetchAudio();
+  const varArr = [...questions];
+  const rightIndexPosition = randomInteger(0, 4);
+  console.log(`right answer: ${currentQuestion.wordTranslate}, position: ${rightIndexPosition + 1}`);
+  choices.forEach((choice, index) => {
+    if (rightIndexPosition === index) {
+      const indexToSplice = varArr.findIndex((elem) => elem.wordTranslate === currentQuestion.wordTranslate);
+      choice.innerHTML = varArr.splice(indexToSplice, 1)[0].wordTranslate;
+    } else {
+      let randomPosition = randomInteger(0, varArr.length - 1);
+      const rightAnswerPosition = varArr.findIndex((elem) => elem.wordTranslate === currentQuestion.wordTranslate);
+      while (randomPosition === rightAnswerPosition) {
+        randomPosition = randomInteger(0, varArr.length - 1);
+      }
+      choice.innerHTML = varArr.splice(randomPosition, 1)[0].wordTranslate;
+    }
   });
   availableQuestions.splice(questionIndex, 1);
   
@@ -99,7 +112,6 @@ const getNewQuestion = () => {
   });
 };
 
-
 const dontKnowAction = () => {
   const rightAnswer = currentQuestion.wordTranslate;
 
@@ -107,16 +119,35 @@ const dontKnowAction = () => {
     if (choice.innerHTML == rightAnswer) {
       choice.classList.add('correct');
       question.innerHTML = currentQuestion.word;
+      transcription.innerHTML = currentQuestion.transcription;
     } else {
       choice.classList.add('disable');
     }
     choice.removeEventListener('click', chooseVariant);
   });
-  
+  iconCircle.classList.add('changeSize');
   btn.innerHTML = 'Next';
   btn.removeEventListener('click', dontKnowAction);
   btn.addEventListener('click', getNewQuestion);
 }
 
-
+const incrementScore = (num)=>{
+  score += num;
+  scoreCounterText.innerText = `${score}/100`;
+}
 btn.addEventListener('click', dontKnowAction);
+
+const fetchAudio =()=>{
+  fetch(`https://raw.githubusercontent.com/vonkrolock/rslang-data/master/${currentQuestion.audio}`)
+  .then((audioResource) => {
+    const audio = new Audio();
+    audio.src = audioResource.url;
+    audio.play();
+  })
+  .catch((error) => console.log(error));  
+}
+
+iconCircle.addEventListener('click',fetchAudio);
+
+fetchWords();
+
